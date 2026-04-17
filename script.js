2bebc19e34dd9851a6060f92a40e4abcc184c958
MateusSRodrigues <mateus50c13d4d3@gmail.com>
2026-04-17 11:03:34 -0300

    v0.1

---
+++ script.js
@@ -0,0 +1,15 @@
+function verificarSenha() {
+    let senha = document.getElementById("senha").value;
+    let resultado = document.getElementById("resultado");
+
+    if (senha.length < 6) {
+        resultado.textContent = "Senha fraca 😬";
+        resultado.style.color = "red";
+    } else if (senha.match(/[0-9]/) && senha.match(/[!@#$%]/)) {
+        resultado.textContent = "Boa! Essa é segura 🔐";
+        resultado.style.color = "green";
+    } else {
+        resultado.textContent = "Senha média. Adicione números e símbolos.";
+        resultado.style.color = "orange";
+    }
+}
